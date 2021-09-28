3bf65e551d3bde9019ead0dabc63340329ab9ffd
Azhar Hussain <shkazharhussain@gmail.com>
2021-09-28 13:07:24 +0530

    "my first python program"

---
+++ prg1.py
@@ -0,0 +1,16 @@
+no1 : int=  20
+no2 : int = 40
+no3 : int = 30
+if(no1>no2>no3):
+    print("largest no1")
+elif(no2>no3>no1):
+    print("largest no2")
+else:
+    print("largest no3")
+
+
+
+
+
+
+
